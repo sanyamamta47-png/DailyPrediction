cef70b018b1e299565e9e5cd6512ea447aefa81c
sanyamamta47-png <sanyamamta47@gmail.com>
2026-06-04 14:26:35 +0530

    SUPABASE

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ PAYMENT_GATEWAY_URL = "https://rzp.io/rzp/7pdLRmod"
 RAZORPAY_KEY_ID = st.secrets["RAZORPAY_KEY_ID"]
 RAZORPAY_KEY_SECRET = st.secrets["RAZORPAY_KEY_SECRET"]
 
-SUPABASE_URL = st.secrets["https://byuxefastgkysmwjpchr.supabase.co"]
-SUPABASE_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dXhlZmFzdGdreXNtd2pwY2hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTg1NDcsImV4cCI6MjA5NjEzNDU0N30.YUBTrLTfs1-NSB1jeSLmvNx0b52kKQ1L4Dqlt7y2gxw"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 @st.cache_resource
 def init_supabase():
